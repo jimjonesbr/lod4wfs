1c00ca950eaa6a8baa372f1ba021552d6ee2735f
jones <jim.jones@uni-muenster.de>
2014-07-22 10:50:51 +0200

    Bug fixes for GeoJSON conversion (negative coordinates).

--- lod4wfs/src/main/java/de/ifgi/lod4wfs/core/Utils.java
+++ lod4wfs/src/main/java/de/ifgi/lod4wfs/core/Utils.java
@@ -10,133 +10,142 @@ import it.cutruzzula.lwkt.WKTParser;
 public class Utils {
 
 	public static String convertWKTtoGML(String literal){
-		
+
 		String gml = new String();
-		
+
 		if(isWKT(literal)){
-			
+
 			try {
-			
+
 				if(literal.contains("<") && literal.contains(">")){
 					String CRS = new String();
-				
-					
+
+
 					//Extracting Reference System
 					if(literal.contains("<") && literal.contains(">")){
-						
+
 						CRS = literal.substring(literal.indexOf("<") + 1, literal.indexOf(">"));
 						literal = literal.substring(literal.indexOf(">") + 1, literal.length());
-						
+
 					}
-					
+
 					//Removing Literal Type
 					if(literal.contains("^^")){
-						
+
 						literal = literal.substring(0, literal.indexOf("^^"));
-						
+
 					}
-									
+
 					gml = WKTParser.parseToGML2(literal,CRS);
-					
-					
+
+
 				} else {
-				
+
 					gml = WKTParser.parseToGML2(literal,GlobalSettings.defautlCRS);
-				
+
 				}
-			
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return gml;
-		
+
 	}
 
+
 	public static String convertWKTtoGeoJSON(String wkt){
-		
-		
-		
+
+
+
 		if(wkt.contains("<") && wkt.contains(">")){
 			String CRS = new String();
-		
-			
+
+
 			//Extracting Reference System
 			if(wkt.contains("<") && wkt.contains(">")){
-				
+
 				CRS = wkt.substring(wkt.indexOf("<") + 1, wkt.indexOf(">"));
 				wkt = wkt.substring(wkt.indexOf(">") + 1, wkt.length());
-				
+
 			}
-			
+
 			//Removing Literal Type
 			if(wkt.contains("^^")){
-				
+
 				wkt = wkt.substring(0, wkt.indexOf("^^"));
+
+			}
+
+		}
+
+		StringBuilder geoJSONStringBuilder = new StringBuilder();
+		geoJSONStringBuilder.append(wkt.replace("(", "[").replace(")", "]").replace(", ",","));
+		String geoType = geoJSONStringBuilder.substring(0, geoJSONStringBuilder.indexOf("[")).trim();
+		geoJSONStringBuilder.delete(geoJSONStringBuilder.indexOf("[")-1, geoJSONStringBuilder.indexOf("["));
+
+		boolean flagNumber = false;
+		StringBuilder geoJSONOutuput = new StringBuilder();
+
+
+		for (int i = 0; i < geoJSONStringBuilder.length(); i++) {
+
+			if(geoJSONStringBuilder.charAt(i)=='[' ||
+					geoJSONStringBuilder.charAt(i)==']' ||	
+					geoJSONStringBuilder.charAt(i)=='.' ||
+					geoJSONStringBuilder.charAt(i)=='-'){
+
+
+				geoJSONOutuput.append(geoJSONStringBuilder.charAt(i));
+
+			} else {
+
+			if(Character.isDigit(geoJSONStringBuilder.charAt(i)) && flagNumber==false){
+				
+				if (!geoType.toUpperCase().equals("POINT")){
+					
+					//Checking if the coordinate has negative values.
+					if(geoJSONStringBuilder.charAt(i-1)=='-'){
+						//Closes the pair of coordinates with a squared bracket '['
+						geoJSONOutuput.insert(geoJSONOutuput.length()-1, '[');
+						
+					} else {
+					
+						geoJSONOutuput.append("[");
+						
+					}
+				}
+				
+				flagNumber = true;
+
+				geoJSONOutuput.append(geoJSONStringBuilder.charAt(i));
+				
+			} else
+
+				if(Character.isDigit(geoJSONStringBuilder.charAt(i)) && flagNumber==true){
+
+					geoJSONOutuput.append(geoJSONStringBuilder.charAt(i));
+					
+				}
+
+			if(geoJSONStringBuilder.charAt(i)==' '){
+
+				geoJSONOutuput.append(",");
+
+			}
+
+			if(geoJSONStringBuilder.charAt(i)==','){
+
+				geoJSONOutuput.append("],[");
 				
 			}
-							
-		}
-		
-		
-		//String geojson = wkt.replace("(", "[").replace(")", "]").replace(", ",","); 
-		StringBuilder geojson2 = new StringBuilder();
-		geojson2.append(wkt.replace("(", "[").replace(")", "]").replace(", ",","));
-		//String geoType = geojson.substring(0, geojson.indexOf("[")).trim();
-		String geoType = geojson2.substring(0, geojson2.indexOf("[")).trim();
-
-		//geojson = geojson.substring(geojson.indexOf("["),geojson.length()).trim();
-		//geojson2.append(geojson2.substring(geojson2.indexOf("["),geojson2.length()).trim());
-		geojson2.delete(geojson2.indexOf("[")-1, geojson2.indexOf("["));
-		boolean flagNumber = false;
-
-		//String res = new String();
-		StringBuilder res = new StringBuilder();
-
-		//for (int i = 0; i < geojson.length(); i++) {
-		for (int i = 0; i < geojson2.length(); i++) {
-			
-			if(geojson2.charAt(i)=='[' || 
-					geojson2.charAt(i)==']' ||	
-					geojson2.charAt(i)=='.'){
-
-				//res = res + geojson2.charAt(i);
-				res.append(geojson2.charAt(i));
-
-			}  
-
-			if(Character.isDigit(geojson2.charAt(i)) && flagNumber==false){
-				if (!geoType.toUpperCase().equals("POINT")){
-					//res = res + "[";
-					res.append("[");
-				}
-				flagNumber = true;
-				//res = res + geojson2.charAt(i);
-				res.append(geojson2.charAt(i));
-			} else 
-
-				if(Character.isDigit(geojson2.charAt(i)) && flagNumber==true){
-					//res = res + geojson2.charAt(i);
-					res.append(geojson2.charAt(i));
-				}
-
-			if(geojson2.charAt(i)==' '){
-				//res = res + ",";
-				res.append(",");
-
-			} 
-
-			if(geojson2.charAt(i)==','){
-				//res = res + "],[";
-				res.append("],[");
 			}
-
 		}
 
 		if (!geoType.toUpperCase().equals("POINT")){
-			//res = res + "]";
-			res.append("]");
+
+			geoJSONOutuput.append("]");
 		}
 
 		if (geoType.toUpperCase().equals("POINT")){
@@ -154,53 +163,54 @@ public class Utils {
 		} else if (geoType.toUpperCase().equals("GEOMETRYCOLLECTION")){
 			geoType = "GeometryCollection";
 		}
-		
-//		geojson = "{\"type\":\""+ geoType + "\",\"coordinates\":" +res+"},";
+
+		return "{\"type\":\""+ geoType + "\",\"coordinates\":" +geoJSONOutuput+"},";
 
 
-		return  "{\"type\":\""+ geoType + "\",\"coordinates\":" +res+"},";
-
-	
 	}
+
+	//TODO: to be implemented
 	public static boolean isGML(String literal){
-		
+
 		return true;
-		
+
 	}
 
+	//TODO: to be implemented
 	public static boolean isWKT(String literal){
-		
+
 		return true;
-		
+
 	}
 
+	//TODO: to be implemented
 	public static boolean isGeoJSON(String literal){
-		
+
 		return true;
-		
+
 	}
-	
+
 	public static String getCanonicalHostName(){
-		
+
 		String result = new String();
-		
+
 		try {
-			
+
 			Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
 
 			for (int networkInterfaceNumber = 0; en.hasMoreElements(); networkInterfaceNumber++) {
-				
+
 				NetworkInterface intf = en.nextElement();
 				Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses();
 
 				for (int addressNumber = 0; enumIpAddr.hasMoreElements(); addressNumber++) {
-					
+
 					InetAddress ipAddr = enumIpAddr.nextElement();
-					
+
 					if(networkInterfaceNumber==2 && addressNumber==1){
 
 						result = ipAddr.getCanonicalHostName();
-						
+
 					}
 				}
 
