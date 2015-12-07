dab6d48c584f82cd8e568757b71cd11dd21aa38b
Jim Jones <jimjonesbr@gmail.com>
2015-12-07 13:14:04 +0100

    Missing file for feature TOC

---
+++ lod4wfs/src/main/java/de/ifgi/lod4wfs/core/WFSFeatureContents.java
@@ -0,0 +1,32 @@
+package de.ifgi.lod4wfs.core;
+
+public class WFSFeatureContents {
+
+
+	private String field;
+	private String fieldType;
+	
+	
+	public WFSFeatureContents() {
+		
+		super();	
+
+	}
+	
+	public String getField() {
+		return field;
+	}
+	
+	public void setField(String field) {
+		this.field = field;
+	}
+	
+	public String getFieldType() {
+		return fieldType;
+	}
+	
+	public void setFieldType(String fieldType) {
+		this.fieldType = fieldType;
+	}
+
+}
